86065a0c987cdb44bd6bfb5dc688362e3b3d9352
wanjiasu <11630918+wanjiasu@user.noreply.gitee.com>
2025-12-07 15:16:51 +0800

    add: pm2 file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name   : "tele_agent_eval",
-    script : "env/bin/uvicorn",
-    args   : "fastapi_app:app --host 0.0.0.0 --port 8004",
+    script : "env/bin/python",
+    args   : "-m uvicorn fastapi_app:app --host 0.0.0.0 --port 8004",
   }]
 }
